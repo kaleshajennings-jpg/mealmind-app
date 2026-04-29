301374c209a471d493328b3df603edf9140c1e02
kaleshajennings-jpg <kaleshajennings@gmail.com>
2026-04-28 21:21:56 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { MEAL_DB, CUISINE_OPTIONS, DIET_OPTIONS, pickMeal, buildWeeklyPlan, buildGroceryList } from './mealData';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
